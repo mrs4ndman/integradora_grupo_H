0baf4abf6376637d5095c206094eb5efd2e3c976
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2025-04-08 10:41:06 +0200

    add: base de proyecto de IntelliJ

---
+++ src/main/java/org/grupo_h/practica_integradora/PracticaIntegradoraApplication.java
@@ -0,0 +1,13 @@
+package org.grupo_h.practica_integradora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaIntegradoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaIntegradoraApplication.class, args);
+    }
+
+}
