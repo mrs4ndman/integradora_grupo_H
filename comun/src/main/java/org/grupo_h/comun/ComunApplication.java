265980b9972687665aeb53dda42ddd19d20bc00f
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2025-04-14 15:51:02 +0200

    add: modulo comun y dependencias de Gradl
    Se cambia el sistema de dependencias de Gradle a un settings.gradle
    grande del que cuelgan 3 pequeños.

---
+++ comun/src/main/java/org/grupo_h/comun/ComunApplication.java
@@ -0,0 +1,13 @@
+package org.grupo_h.comun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComunApplication.class, args);
+    }
+
+}
