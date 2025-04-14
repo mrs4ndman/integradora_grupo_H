6e4962dce6ccf80608296f165cb3d24eb13d3b21
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2025-04-14 20:54:53 +0200

    add: outline de entidades y relaciones

---
+++ empleados/src/main/java/org/grupo_h/empleados/entity/Mensaje.java
@@ -0,0 +1,33 @@
+package org.grupo_h.empleados.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Mensaje {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Past
+    private LocalDateTime fechaEmision;
+
+    @OneToOne
+    private Empleado emisor;
+
+    @OneToOne
+    private Empleado receptor;
+
+    @NotBlank
+    private String mensaje;
+
+
+}
---
+++ empleados/src/main/java/org/grupo_h/empleados/entity/PeriodoColaboracion.java
@@ -0,0 +1,15 @@
+package org.grupo_h.empleados.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class PeriodoColaboracion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
