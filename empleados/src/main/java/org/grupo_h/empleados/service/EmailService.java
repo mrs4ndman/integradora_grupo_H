3f25fb22b859ebaa7f7089788d564ec602fd6d17
PVBLS <pablolink@hotmail.es>
2025-04-23 13:24:28 +0200

    add: Servicio email para recuperacion contraseña
    Issue #20
    Queda implementar el envio de contraseña olvidada

---
+++ empleados/src/main/java/org/grupo_h/empleados/service/EmailService.java
@@ -0,0 +1,7 @@
+package org.grupo_h.empleados.service;
+
+import org.grupo_h.comun.entity.auxiliar.EmailDetalles;
+
+public interface EmailService {
+    // Enviar un email sencillo
+    String enviarEmail(EmailDetalles details);}
