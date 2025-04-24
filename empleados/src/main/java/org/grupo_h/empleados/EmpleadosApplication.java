73560f6a3a8282d1ac793c7a85b907bf59c925cb
Jose Antonio del Pino <jadelpino10@gmail.com>
2025-04-25 00:50:36 +0200

    add: Ajuste de campos y adición entidades nuevas
    + Sólo esta hecha la estructura de BB.DD. Faltan repositorios y
      Servicios
    + Campos añadidos y retocados en Entidades prexistentes conforme a los
      nuevos enunciados recibidos en issue (#26)
    + Nuevas Entidades y campos añadidos en relación a los Datos Económicos
    + Model Mapper añadido a la espera de confirmación por los otros
      integrantes del equipo

--- empleados/src/main/java/org/grupo_h/empleados/EmpleadosApplication.java
+++ empleados/src/main/java/org/grupo_h/empleados/EmpleadosApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.RepositoryDefinition;
 
 @SpringBootApplication
 // Escanear entidades en el módulo Comun
